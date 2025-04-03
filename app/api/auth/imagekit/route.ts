26dde30e65c898ecf05011c24e257dc2568e8b73
Arkadeep Bag <arkadeepbag@Arkadeeps-MacBook-Air.local>
2025-04-04 00:19:03 +0530

    first commit

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -8,7 +8,7 @@ const {
   },
 } = config;
 
-const imagekit = new ImageKit({ publicKey, privateKey, urlEndpoint });
+const imagekit = new ImageKit({ publicKey:process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!, privateKey:process.env.IMAGEKIT_PRIVATE_KEY!, urlEndpoint:process.env.NEXT_PUBLIC_IMAGEKIT_URL! });
 export async function GET() {
   console.log("--->",urlEndpoint,privateKey,publicKey)
   return NextResponse.json(imagekit.getAuthenticationParameters());
