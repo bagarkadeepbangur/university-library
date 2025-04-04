d3bd52b0d03216a5b4c30bfa55a60650efd1bf62
Arkadeep Bag <arkadeepbag@Arkadeeps-MacBook-Air.local>
2025-04-05 00:06:26 +0530

    implement email workflows

--- app/api/imagekit/auth/route.ts
+++ app/api/imagekit/auth/route.ts
@@ -1,7 +1,7 @@
 import ImageKit from "imagekit";
 import config from "@/lib/config";
 // import { NextResponse } from "next/server";
-const allowedOrigin = "https://university-library-eoiywvfdt-arkadeep-bags-projects.vercel.app";
+// const allowedOrigin = "https://university-library-eoiywvfdt-arkadeep-bags-projects.vercel.app";
 const {
   env: {
     imagekit: { publicKey, privateKey, urlEndpoint },
@@ -10,16 +10,21 @@ const {
 
 // console.log("--->",urlEndpoint,privateKey,publicKey,process.env.NEXT_PUBLIC_IMAGEKIT_URL!)
 const imagekit = new ImageKit({ publicKey:process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!, privateKey:process.env.IMAGEKIT_PRIVATE_KEY!, urlEndpoint:process.env.NEXT_PUBLIC_IMAGEKIT_URL! });
-export async function GET() {
+
+function getRequestOrigin(request: Request) {
+  const origin = request.headers.get("origin");
+  return origin || "*";
+}
+export async function GET(request: Request) {
 
   // return NextResponse.json(imagekit.getAuthenticationParameters());
-
+  const origin = getRequestOrigin(request);
   const authParams =imagekit.getAuthenticationParameters()
 
   return new Response(JSON.stringify(authParams), {
     status: 200,
     headers: {
-      "Access-Control-Allow-Origin": allowedOrigin,
+      "Access-Control-Allow-Origin": origin,
       "Access-Control-Allow-Methods": "GET, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type",
       "Content-Type": "application/json",
@@ -28,11 +33,12 @@ export async function GET() {
 }
 
 // Handle OPTIONS (preflight)
-export async function OPTIONS() {
+export async function OPTIONS(request: Request) {
+  const origin = getRequestOrigin(request);
   return new Response(null, {
     status: 204,
     headers: {
-      "Access-Control-Allow-Origin": allowedOrigin,
+      "Access-Control-Allow-Origin": origin,
       "Access-Control-Allow-Methods": "GET, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type",
     },
