89da9c285a9c67a9e3f2286b2eaa1aebf9b7d701
Arkadeep Bag <arkadeepbag@Arkadeeps-MacBook-Air.local>
2025-04-04 23:50:01 +0530

    implement email workflows

--- app/api/imagekit/auth/route.ts
+++ app/api/imagekit/auth/route.ts
@@ -8,9 +8,32 @@ const {
   },
 } = config;
 
-console.log("--->",urlEndpoint,privateKey,publicKey,process.env.NEXT_PUBLIC_IMAGEKIT_URL!)
+// console.log("--->",urlEndpoint,privateKey,publicKey,process.env.NEXT_PUBLIC_IMAGEKIT_URL!)
 const imagekit = new ImageKit({ publicKey:process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!, privateKey:process.env.IMAGEKIT_PRIVATE_KEY!, urlEndpoint:process.env.NEXT_PUBLIC_IMAGEKIT_URL! });
 export async function GET() {
-  console.log("--->",urlEndpoint,privateKey,publicKey)
-  return NextResponse.json(imagekit.getAuthenticationParameters());
+
+  // return NextResponse.json(imagekit.getAuthenticationParameters());
+
+  const authParams =imagekit.getAuthenticationParameters()
+
+  return new Response(JSON.stringify(authParams), {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Replace * with specific domain in production
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
+}
+
+// Handle OPTIONS (preflight)
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
 }
