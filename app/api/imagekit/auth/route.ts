b369772a68baa061c976bdccc09688f7ee53fa1e
Arkadeep Bag <arkadeepbag@Arkadeeps-MacBook-Air.local>
2025-04-05 22:57:31 +0530

    making image upload not requeired

--- app/api/imagekit/auth/route.ts
+++ app/api/imagekit/auth/route.ts
@@ -25,7 +25,7 @@ export async function GET(request: Request) {
     status: 200,
     headers: {
       "Access-Control-Allow-Origin": origin,
-      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Methods": "GET,POST, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type",
       "Content-Type": "application/json",
     },
